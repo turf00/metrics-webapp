ba12096966df26af2e19263b5ba08b71638b84cf
Justin McCartney <justin.mccartney@gmail.com>
2019-05-27 16:58:12 +0100

    + Add random path generator for the perf tests.

--- contrast-perftest/src/test/scala/metric-webapp/BasicSimulation.scala
+++ contrast-perftest/src/test/scala/metric-webapp/BasicSimulation.scala
@@ -6,6 +6,21 @@ import scala.concurrent.duration._
 
 class BasicSimulation extends Simulation {
 
+  private val rand = scala.util.Random
+  private val feeder = Iterator.continually(generateUrls)
+
+  private def generateUrls: Map[String,String] = {
+    Map("accountUrl" -> accountUrl(), "transactionUrl" -> transUrl())
+  }
+
+  private def transUrl(): String = {
+    accountUrl() + "/transactions/" + rand.nextInt(Integer.MAX_VALUE)
+  }
+
+  private def accountUrl(): String = {
+    "/accounts/" + rand.nextInt(Integer.MAX_VALUE)
+  }
+
   val httpProtocol = http
     .baseUrl("http://localhost:8080")
     .acceptHeader("application/json")
@@ -13,11 +28,12 @@ class BasicSimulation extends Simulation {
     .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:16.0) Gecko/20100101 Firefox/16.0")
 
   val scn = scenario("GetAccount and Transactions") // A scenario is a chain of requests and pauses
+    .feed(feeder)
     .exec(http("GetAccount")
-      .get("/accounts/34567"))
+      .get("${accountUrl}"))
     .pause(1) // Note that Gatling has recorder real time pauses
     .exec(http("GetTransactions")
-      .get("/accounts/34567/transactions/12345"))
+      .get("${transactionUrl}"))
 
   //setUp(scn.inject(atOnceUsers(10) during 300s).protocols(httpProtocol))
   setUp(scn.inject(rampUsersPerSec(10) to 500 during 30).protocols(httpProtocol))
